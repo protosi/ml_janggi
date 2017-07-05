73e5f30aa425dedf6c5dc07a285c8a0367a09fe3
전준형 <midtics@com2us.com>
2017-07-05 11:22:02 +0900

    유닛 쫄 이동규칙 제작

--- ANN_EXAM_01/janggi/UnitJol.py
+++ ANN_EXAM_01/janggi/UnitJol.py
@@ -5,6 +5,7 @@ Created on 2017. 6. 15.
 '''
 
 from Unit import Unit
+from Pos import Pos
 
 class UnitJol(Unit):
 
@@ -15,7 +16,84 @@ class UnitJol(Unit):
         self.setScore(400);
         
     
-    def getPossibleMoveList(self):
+    def getPossibleMoveList(self, map):
         pos = self.getPos();
       
+        # 연산을 위해 위치 정보를 획득한다.
+        pos = self.getPos()
+        now_x = pos.getXPos()
+        now_y = pos.getYPos()
         
+        # 현재 맵을 불러온다
+        current_map = self.game.getMap()
+        
+        # 리턴할 빈 맵을 하나 생성한다.
+        map = self.game.getEmptyMap()
+        
+        # Pos만 리턴할껀디
+        list = [];
+        
+        # 좌측 이동 
+        if(now_x > 0 and (current_map[now_y][now_x-1] == 0 or current_map[now_y][now_x-1].getFlag() != self.getFlag())):
+            map[now_y][now_x-1] = 1
+            list.append(Pos(now_x-1, now_y))
+        
+        # 우측 이동
+        if((now_x < len(current_map[now_y] ) -1)  and (current_map[now_y][now_x+1] == 0 or current_map[now_y][now_x+1].getFlag() != self.getFlag())):
+            map[now_y][now_x+1] = 1
+            list.append(Pos(now_x+1, now_y))
+            
+        # 초의 쫄은 내려온다.
+        if(self.getFlag() == 1):
+            if(now_y < len(current_map) -1 and (current_map[now_y+1][now_x] == 0 or current_map[now_y+1][now_x].getFlag() != self.getFlag())):
+                map[now_y+1][now_x] = 1
+                list.append(Pos(now_x, now_y+1))
+            
+        # 한의 쫄은 올라간다.
+        elif(self.getFlag() == 2):
+            if(now_y > 0 and (current_map[now_y-1][now_x] == 0 or current_map[now_y-1][now_x].getFlag() != self.getFlag())):
+                map[now_y-1][now_x] = 1
+                list.append(Pos(now_x, now_y-1))
+        
+        # 대각선 처리
+        # 상단 - 초의 진형
+        if(now_y < 3):    
+            if(now_x - now_y == 3):
+                if(now_y == 1):
+                    if(current_map[now_y-1][now_x-1] == 0 or current_map[now_y-1][now_x-1].getFlag() != self.getFlag()):
+                        map[now_y-1][now_x-1] = 1
+                        list.append(Pos(now_x-1, now_y-1))
+                if(now_y == 2):
+                    if(current_map[now_y-1][now_x-1] == 0 or current_map[now_y-1][now_x-1].getFlag() != self.getFlag()):
+                        map[now_y-1][now_x-1] = 1
+                        list.append(Pos(now_x-1, now_y -1))
+            if(now_x+now_y == 5):
+                if(now_y == 1):
+                    if(current_map[now_y-1][now_x+1] == 0 or current_map[now_y-1][now_x+1].getFlag() != self.getFlag()):
+                        map[now_y-1][now_x+1] = 1
+                        list.append(Pos(now_x+1, now_y-1))
+                        
+                if(now_y == 2):
+                    if(current_map[now_y-1][now_x+1] == 0 or current_map[now_y-1][now_x+1].getFlag() != self.getFlag()):
+                        map[now_y-1][now_x+1] = 1
+                        list.append(Pos(now_x+1, now_y-1))
+                        
+        # 하단 - 한의 진형
+        elif(now_y > 6):  
+            if(now_y - now_x == 4):
+                if(now_y == 7):   
+                    if(current_map[now_y+1][now_x+1] == 0 or current_map[now_y+1][now_x+1].getFlag() != self.getFlag()):   
+                        map[now_y+1][now_x+1] = 1
+                        list.append(Pos(now_x+1, now_y+1))
+                        
+                if(now_y == 8):
+                    if(current_map[now_y+1][now_x+1] == 0 or current_map[now_y+1][now_x+1].getFlag() != self.getFlag()):
+                        map[now_y+1][now_x+1] = 1
+                        list.append(Pos(now_x+1, now_y+1))
+                        
+            if(now_x + now_y == 12):
+                if(now_y == 7):
+                    if(current_map[now_y+1][now_x-1] == 0 or current_map[now_y+1][now_x-1].getFlag() != self.getFlag()):
+                        map[now_y+1][now_x-1] = 1
+                        list.append(Pos(now_x-1, now_y+1)) 
+        return map, list
