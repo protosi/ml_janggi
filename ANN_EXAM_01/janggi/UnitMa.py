d6888b3be58a004b2ed5f6efacf192fb0cf5e953
전준형 <midtics@com2us.com>
2017-07-05 17:12:30 +0900

    유닛 (마)도 끝

--- ANN_EXAM_01/janggi/UnitMa.py
+++ ANN_EXAM_01/janggi/UnitMa.py
@@ -5,6 +5,7 @@ Created on 2017. 6. 15.
 '''
 
 from Unit import Unit
+from Pos import Pos
 
 class UnitMa(Unit):
 
@@ -15,7 +16,71 @@ class UnitMa(Unit):
         self.setScore(700);
         
     
-    def getPossibleMoveList(self):
-        pos = self.getPos();
-      
+    def getPossibleMoveList(self, map):
+        # 연산을 위해 위치 정보를 획득한다.
+        pos = self.getPos()
+        now_x = pos.getXPos()
+        now_y = pos.getYPos()
         
+        # 현재 맵을 불러온다
+        current_map = self.game.getMap()
+        
+        # 리턴할 빈 맵을 하나 생성한다.
+        map = self.game.getEmptyMap()
+        
+        # Pos만 리턴할껀디
+        list = [];
+      
+        # 상단 이동
+        if now_y > 1:
+            if current_map[now_y-1][now_x] == 0:
+                if now_x > 0 and (current_map[now_y-2][now_x-1] == 0 or current_map[now_y-2][now_x-1].getFlag() != self.getFlag()):
+                    # 좌상 이동
+                    map[now_y-2][now_x-1] = 1
+                    list.append(Pos(now_x-1, now_y-2))
+                    
+                if now_x < 8 and (current_map[now_y-2][now_x+1] == 0 or current_map[now_y-2][now_x+1].getFlag() != self.getFlag()):
+                    # 우상 이동
+                    map[now_y-2][now_x+1] = 1
+                    list.append(Pos(now_x+1, now_y-2))
+                    
+        # 하단 이동
+        if now_y < 8:
+            if current_map[now_y+1][now_x] == 0:
+                if now_x > 0 and (current_map[now_y+2][now_x-1] == 0 or current_map[now_y+2][now_x-1].getFlag() != self.getFlag()):
+                    # 좌하 이동
+                    map[now_y+2][now_x-1] = 1
+                    list.append(Pos(now_x-1, now_y+2))
+                    
+                if now_x < 8 and (current_map[now_y+2][now_x+1] == 0 or current_map[now_y+2][now_x+1].getFlag() != self.getFlag()):
+                    # 우하 이동
+                    map[now_y+2][now_x+1] = 1
+                    list.append(Pos(now_x+1, now_y+2))
+                    
+        # 좌측 이동
+        if now_x > 1:
+            if current_map[now_y][now_x-1] == 0:
+                if now_y > 0 and (current_map[now_y-1][now_x-2] == 0 or current_map[now_y-1][now_x-2].getFlag() != self.getFlag()):
+                    # 좌상 이동
+                    map[now_y-1][now_x-2] = 1
+                    list.append(Pos(now_x-2, now_y-1))
+                    
+                if now_y < 9 and (current_map[now_y+1][now_x-2] == 0 or current_map[now_y+1][now_x-2].getFlag() != self.getFlag()):
+                    # 좌하 이동
+                    map[now_y+1][now_x-2] = 1
+                    list.append(Pos(now_x-2, now_y-1))
+        
+        # 우측 이동
+        if now_x < 7:
+            if current_map[now_y][now_x+1] == 0:
+                if now_y > 0 and (current_map[now_y-1][now_x+2] == 0 or current_map[now_y-1][now_x+2].getFlag() != self.getFlag()):
+                    # 우상 이동    
+                    map[now_y-1][now_x+2] = 1
+                    list.append(Pos(now_x+2, now_y-1))
+                    
+                if now_y < 9 and (current_map[now_y+1][now_x+2] == 0 or current_map[now_y+1][now_x+2].getFlag() != self.getFlag()):
+                    # 우하 이동
+                    map[now_y+1][now_x+2] = 1
+                    list.append(Pos(now_x+2, now_y+1))
+                    
+        return map, list
